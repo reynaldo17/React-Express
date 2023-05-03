d3166517b74f68f0757d875730d3f8e168a52d22
Reynaldo Estrella <reynaldo@Reynaldos-MBP.lehman.edu>
2023-05-03 12:45:05 -0400

    Changes to index.js in server folder

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Im sending something yo!')
+})
+
 app.get('/api', (req, res) => {
     res.json({message: 'Yo my boi this is the server'})
 })
